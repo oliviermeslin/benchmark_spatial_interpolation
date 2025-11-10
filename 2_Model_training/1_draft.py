c3f2eb3402e7328bf395eb4711b0de6dd134e711
oliviermeslin <olivier.meslin@insee.fr>
2025-11-10 17:30:00 +0000

    Add first draft

--- 2_Model_training/1_draft.py
+++ 2_Model_training/1_draft.py
@@ -2,6 +2,7 @@
 import os
 import s3fs
 from utils import s3
+import time
 
 # Modules for data manipulation
 import polars as pl
@@ -10,8 +11,10 @@ import numpy as np
 
 # Modules for machine learning
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
-
+from sklearn.metrics import r2_score
+from utils.functions import AddCoordinatesRotation
 
 # %%
 # Connect to S3
@@ -21,5 +24,62 @@ fs = s3fs.S3FileSystem(anon=False)  # set anon=True for public buckets
 datapath = 's3://projet-benchmark-spatial-interpolation/data'
 
 # %%
-temp = s3.get_df_from_s3(f"{datapath}/real/RGEALTI/RGEALTI_parquet/")
+# Extract one departement
+data = (
+    s3.get_df_from_s3(f"{datapath}/real/BDALTI/BDALTI_parquet/")
+    .filter(c.departement == "01")
+    .filter(~c.value.is_nan())
+    .select("x", "y", "value")
+    .collect()
+)
+# %%
+# Separate target and features
+X = data.select("x", "y")
+y = data.select("value").to_numpy().ravel()
 
+# %%
+# Perform a train test split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=123
+)
+
+# %%
+# Instantiate the model
+rf_model = RandomForestRegressor(random_state=42, n_jobs=-1)
+
+full_model = Pipeline(
+    [
+        ("coord_rotation", AddCoordinatesRotation()),
+        ("ml_model", rf_model)
+    ]
+)
+# %%
+# Define model hyperparameters
+parameters_model = {
+    "coord_rotation__coordinates_names": ("x", "y"),
+    "coord_rotation__number_axis": 23,
+    "ml_model__n_estimators": 50,
+    "ml_model__max_features": "sqrt",
+    "ml_model__min_samples_split": 40,
+    "ml_model__min_samples_leaf": 20,
+    "ml_model__verbose": 3,
+    "ml_model__oob_score": True
+}
+
+full_model.set_params(**parameters_model)
+
+# %%
+# Entraînement du modèle
+start_time = time.perf_counter()
+full_model.fit(X_train, y_train)
+end_time = time.perf_counter()
+elapsed_time = end_time - start_time
+print(f"Training time: {elapsed_time:.2f} secondes")
+
+# %%
+print(rf_model.oob_score_)
+
+y_pred = full_model.predict(X_test)
+r2_test = r2_score(y_test, y_pred)
+print(r2_test)
+# %%
---
+++ utils/functions.py
@@ -0,0 +1,156 @@
+import math
+import polars as pl
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+def rotate_point(x, y, angle, center=None):
+    """
+    Rotate a 2D point counterclockwise by a given angle (in degrees) around a given center.
+
+    Parameters:
+    x (float or np.array): x-coordinate(s) to rotate.
+    y (float or np.array): y-coordinate(s) to rotate.
+    angle (float): Angle in degrees by which to rotate the point.
+    center (tuple, optional): Center of rotation as (cx, cy). Defaults to the origin (0, 0).
+
+    Returns:
+    tuple: Rotated x and y coordinates.
+    """
+    cx, cy = center if center else (0, 0)
+    dx = x - cx
+    dy = y - cy
+    ang_rad = math.radians(angle)
+
+    xx = cx + dx * math.cos(ang_rad) - dy * math.sin(ang_rad)
+    yy = cy + dx * math.sin(ang_rad) + dy * math.cos(ang_rad)
+
+    return xx, yy
+
+
+# A custom transformer to rotate geographical coordinates an arbitrary number of times
+class AddCoordinatesRotation(BaseEstimator, TransformerMixin):
+    """
+    A custom transformer to rotate geographical coordinates an arbitrary number of times.
+
+    Parameters:
+    coordinates_names (tuple): Tuple of column names representing the x and y coordinates.
+    number_axis (int): The number of rotations to apply to the coordinates.
+    """
+    def __init__(self, coordinates_names: tuple = None, number_axis: int = None):
+        self.coordinates_names = coordinates_names
+        self.number_axis = number_axis
+        self.is_fitted = False
+
+    def set_params(self, coordinates_names: tuple = None, number_axis: int = None):
+        """
+        Set parameters for the transformer.
+
+        Parameters:
+        coordinates_names (tuple): Tuple of column names representing the x and y coordinates.
+        number_axis (int): The number of rotations to apply to the coordinates.
+
+        Returns:
+        self
+        """
+        self.coordinates_names = coordinates_names
+        self.rotated_coordinates_names = []
+        self.number_axis = number_axis
+        return self
+
+    def fit(self, X: pl.DataFrame, y=None):
+        """
+        Fit the transformer by checking for valid coordinate names and calculating the mean center.
+
+        Parameters:
+        X (pl.DataFrame): Input data.
+        y (optional): Target values, not used in fitting.
+
+        Returns:
+        self
+        """
+        assert isinstance(X, pl.DataFrame), "X must be a Polars DataFrame"
+        coordinates_names = self.coordinates_names
+
+        # Raise an error if the coordinates are not correct
+        if coordinates_names is None:
+            raise ValueError("Argument coordinates_names is missing.")
+        if len(coordinates_names) != 2:
+            raise ValueError("There must be exactly two coordinates.")
+        if coordinates_names[0] not in X.columns:
+            raise ValueError(f"Coordinate {coordinates_names[0]} is not in the data.")
+        if coordinates_names[1] not in X.columns:
+            raise ValueError(f"Coordinate {coordinates_names[1]} is not in the data.")
+
+        # Raise an error if the number of axis is missing
+        if self.number_axis is None:
+            raise ValueError("Argument number_axis is missing.")
+
+        x_coord, y_coord = self.coordinates_names
+        # Compute the mean coordinates of the data
+        self.center = (X[x_coord].mean(), X[y_coord].mean())
+
+        self.is_fitted = True
+        return self
+
+    def transform(self, X: pl.DataFrame, y=None):
+        """
+        Rotate the coordinates and return the modified data.
+
+        Parameters:
+        X (pl.DataFrame): Input data.
+        y (optional): Target values, not used in transformation.
+
+        Returns:
+        pl.DataFrame: Transformed data with additional rotated coordinates.
+        """
+        assert isinstance(X, pl.DataFrame), "X must be a Polars DataFrame"
+        x_coord, y_coord = self.coordinates_names
+        rotated_coordinates_names = [x_coord, y_coord]
+
+        for i in range(1, self.number_axis):
+            # Compute coordinates after rotation
+            x_temp, y_temp = rotate_point(
+                x=X[x_coord],
+                y=X[y_coord],
+                angle=360 * (i / self.number_axis),
+                center=self.center
+            )
+
+            # Add the rotated coordinates to the data
+            X = X.with_columns(
+                [
+                    x_temp.alias(f"{x_coord}_rotated{i}"),
+                    y_temp.alias(f"{y_coord}_rotated{i}")
+                ]
+            )
+            rotated_coordinates_names = rotated_coordinates_names + [
+                f"{x_coord}_rotated{i}", f"{y_coord}_rotated{i}"
+            ]
+
+        self.rotated_coordinates_names = rotated_coordinates_names
+        self.names_features_output = X.columns
+        return X
+
+    def fit_transform(self, X, y=None):
+        """
+        Fit and transform the data in one step.
+
+        Parameters:
+        X (pl.DataFrame): Input data.
+        y (optional): Target values, not used in fitting.
+
+        Returns:
+        pl.DataFrame: Transformed data.
+        """
+        self.fit(X, y)
+        return self.transform(X, y)
+
+    def get_feature_names_out(self):
+        """
+        Get the names of the transformed features.
+
+        Returns:
+        list: Names of the transformed features.
+        """
+        return self.names_features_output
+
