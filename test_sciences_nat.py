38846794e30ebec38561fe69b89927ddf33560ab
oliviermeslin <olivier.meslin@insee.fr>
2026-02-24 10:26:45 +0000

    WIP

--- test_sciences_nat.py
+++ test_sciences_nat.py
@@ -4,6 +4,8 @@
 import polars as pl
 from polars import col as c
 from datetime import datetime
+import time
+import random
 
 # Modules for machine learning
 import lightgbm
@@ -28,23 +30,29 @@ df = (
 
 # %%
 # Prepare the data
+noises = [random.random() for i in range(df.shape[0])]
+
 df2 = (
     df
+    .with_columns(noise = pl.Series(noises))
     .with_columns(
         date=c.date.cast(pl.Utf8).str.to_date("%Y%m%d"),
         hours_since_ref=(
             (pl.col("valid_time") - pl.lit(datetime(2025, 1, 1, 0)))
             .dt.total_hours()
         ),
-        temperature_celsius=c.t2m - 273.15
+        temperature_celsius=c.t2m - 273.15,
+        temperature_celsius_noisy=c.t2m - 273.15 + 3 * (c.noise - 0.5)
     )
-    .select("latitude", "longitude", "hours_since_ref", "temperature_celsius")
+    .select("latitude", "longitude", "hours_since_ref", "temperature_celsius", "temperature_celsius_noisy")
 )
 
 # %%
 # Perform the train-test split
 train_val, test = train_test_split(
-    df2,
+    df2.filter(
+        c.hours_since_ref <= 180
+    ),
     test_size=0.15,
     random_state=20230516
 )
@@ -89,7 +97,7 @@ parameters_baseline_model = {
     "coord_rotation__coordinates_names": ("latitude", "longitude"),
     "coord_rotation__number_axis": 1,
     "model__seed": 20230516,
-    "model__n_estimators": 1000,
+    "model__n_estimators": 10000,
     # Very important: 0.12 is optimal, but 0.2 is much faster
     "model__learning_rate": 0.2,
     "model__num_leaves": 1023,
@@ -109,27 +117,30 @@ baseline_preprocessor = baseline_model[:-1]
 
 baseline_preprocessor.fit(
     train.select("latitude", "longitude", "hours_since_ref"),
-    train.select("temperature_celsius")
+    train.select("temperature_celsius_noisy")
 )
 train_trans = baseline_preprocessor.transform(train)
 val_trans = baseline_preprocessor.transform(val)
 
 # %%
+start_time = time.monotonic()
 eval_set = [
-    (train_trans, train["temperature_celsius"].to_numpy().ravel()),
-    (val_trans, val["temperature_celsius"].to_numpy().ravel())
+    (train_trans, train["temperature_celsius_noisy"].to_numpy().ravel()),
+    (val_trans, val["temperature_celsius_noisy"].to_numpy().ravel())
 ]
 eval_names = ["Train", "Validation"]
 baseline_model[-1].fit(
     train_trans,
-    train["temperature_celsius"].to_numpy().ravel(),
+    train["temperature_celsius_noisy"].to_numpy().ravel(),
     eval_set=eval_set,
     eval_names=eval_names,
     callbacks=[
-        lightgbm.log_evaluation(period=10),
-        lightgbm.early_stopping(stopping_rounds=20)
+        lightgbm.log_evaluation(period=50),
+        lightgbm.early_stopping(stopping_rounds=10)
     ]
 )
+end_time = time.monotonic()
+print(f"Training time of the baseline model: {end_time - start_time} seconds")
 
 
 
@@ -146,7 +157,7 @@ parameters_full_model = {
     "coord_rotation__coordinates_names": ("latitude", "longitude"),
     "coord_rotation__number_axis": 11,
     "model__seed": 20230516,
-    "model__n_estimators": 1000,
+    "model__n_estimators": 10000,
     # Very important: 0.12 is optimal, but 0.2 is much faster
     "model__learning_rate": 0.2,
     "model__num_leaves": 1023,
@@ -166,26 +177,29 @@ full_preprocessor = full_model[:-1]
 
 full_preprocessor.fit(
     train.select("latitude", "longitude", "hours_since_ref"),
-    train.select("temperature_celsius")
+    train.select("temperature_celsius_noisy")
 )
 train_trans = full_preprocessor.transform(train)
 val_trans = full_preprocessor.transform(val)
 
 # %%
+start_time = time.monotonic()
 eval_set = [
-    (train_trans, train["temperature_celsius"].to_numpy().ravel()),
-    (val_trans, val["temperature_celsius"].to_numpy().ravel())
+    (train_trans, train["temperature_celsius_noisy"].to_numpy().ravel()),
+    (val_trans, val["temperature_celsius_noisy"].to_numpy().ravel())
 ]
 eval_names = ["Train", "Validation"]
 full_model[-1].fit(
     train_trans,
-    train["temperature_celsius"].to_numpy().ravel(),
+    train["temperature_celsius_noisy"].to_numpy().ravel(),
     eval_set=eval_set,
     eval_names=eval_names,
     callbacks=[
-        lightgbm.log_evaluation(period=10),
+        lightgbm.log_evaluation(period=50),
         lightgbm.early_stopping(stopping_rounds=20)
     ]
 )
+end_time = time.monotonic()
+print(f"Training time of the full model: {end_time - start_time} seconds")
 
 # %%
